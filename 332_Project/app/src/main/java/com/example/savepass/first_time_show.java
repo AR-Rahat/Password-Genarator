a5e727281061cd53b4bf2abbfab89a858b09c37c
Fahad-Bin <fahadbin8900@gmail.com>
2021-07-03 21:07:50 +0600

    First page

--- 332_Project/app/src/main/java/com/example/savepass/first_time_show.java
+++ 332_Project/app/src/main/java/com/example/savepass/first_time_show.java
@@ -2,19 +2,13 @@ package com.example.savepass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class first_time_show extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent in = new Intent(this,first_time_show.class);
-        startActivity(in);
-
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_time_show);
     }
 }
