9c48287cda8a5b00c9d2b951dd5f3cffdf843c59
Fahad-Bin <fahadbin8900@gmail.com>
2021-07-08 23:28:54 +0600

    log in & sing up page done

--- 332_Project/app/src/main/java/com/example/savepass/login_page.java
+++ 332_Project/app/src/main/java/com/example/savepass/login_page.java
@@ -2,19 +2,13 @@ package com.example.savepass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent in = new Intent(this,singup_page.class);
-        startActivity(in);
-
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
     }
 }
