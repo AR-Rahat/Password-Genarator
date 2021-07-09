9a9bac6f03200502b2f31c8939b71fe55821c83f
AR-Rahat <asfakurrahat7@gmail.com>
2021-07-10 00:23:51 +0600

    Code rearrange

--- 332_Project/app/src/main/java/com/example/savepass/Homepage.java
+++ 332_Project/app/src/main/java/com/example/savepass/Homepage.java
@@ -1,6 +1,6 @@
 package com.example.savepass;
 
-import androidx.appcompat.app.AppCompatActivity;
+/*import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,42 +15,60 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;*/
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity {
-    FloatingActionButton f_btn;
+
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
+    FloatingActionButton f_btn;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        f_btn=(FloatingActionButton) findViewById(R.id.floating_action_button);
-
-        f_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent l=new Intent(Homepage.this, add_items.class);
-                startActivity(l);
-            }
-        });
-
         setUpToolbar();
         navigationView = (NavigationView) findViewById(R.id.navigation_menu);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId())
-                {
-                    case  R.id.nav_home:
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId())
+            {
+                case  R.id.nav_pass: {
+                    Intent intent = new Intent(Homepage.this, password.class);
+                    startActivity(intent);
+                }
+                    break;
 
-                        Intent intent = new Intent(Homepage.this, Homepage.class);
-                        startActivity(intent);
-                        break;
+                case R.id.nav_notes: {
+                    Intent intent1 = new Intent(Homepage.this, notes.class);
+                    startActivity(intent1);
+                }
+                    break;
+
+                case R.id.nav_address: {
+                    Intent intent2 = new Intent(Homepage.this, address.class);
+                    startActivity(intent2);
+                }
+                    break;
 
 //Paste your privacy policy link
 
@@ -60,21 +78,29 @@ public class Homepage extends AppCompatActivity {
 //                        startActivity(browserIntent);
 //
 //                    }
-                    //       break;
-                    case  R.id.nav_share:{
+                //       break;
+                case  R.id.nav_share:{
 
-                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                        sharingIntent.setType("text/plain");
-                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
-                        String shareSub = "Try now";
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
-                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                    sharingIntent.setType("text/plain");
+                    String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                    String shareSub = "Try now";
+                    sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                    sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                    startActivity(Intent.createChooser(sharingIntent, "Share using"));
 
-                    }
-                    break;
                 }
-                return false;
+                break;
+            }
+            return false;
+        });
+        f_btn=(FloatingActionButton) findViewById(R.id.floating_action_button);
+
+        f_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent l=new Intent(Homepage.this, add_items.class);
+                startActivity(l);
             }
         });
     }
