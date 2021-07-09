15bb12f47f142c8efb83cd52c1f42ae739cf5099
Nazmur Raihan <56562412+Raihan1431@users.noreply.github.com>
2021-07-09 17:41:45 +0600

    welcome screen

--- 332_Project/app/src/main/java/com/example/savepass/MainActivity.java
+++ 332_Project/app/src/main/java/com/example/savepass/MainActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private  static int sto=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent in = new Intent(this,singup_page.class);
-        startActivity(in);
+       //Intent in = new Intent(this,first_time_show.class);
+        // startActivity(in);
 
 
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent homeIntent=new Intent(MainActivity.this, first_time_show.class);
+            startActivity(homeIntent);
+            finish();
+            }
+        },sto);
     }
 }
