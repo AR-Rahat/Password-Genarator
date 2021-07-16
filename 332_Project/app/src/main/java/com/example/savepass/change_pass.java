00dd2e3edca3cd3dc9108b8aabaa87c829e6d753
Fahad-Bin <fahadbin8900@gmail.com>
2021-07-16 21:33:42 +0600

    setting added

---
+++ 332_Project/app/src/main/java/com/example/savepass/change_pass.java
@@ -0,0 +1,14 @@
+package com.example.savepass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class change_pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pass);
+    }
+}
